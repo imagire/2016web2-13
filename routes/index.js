80db1579738048309ec5ddda5841e8397bf806b9
imagire <imagire@gmail.com>
2016-12-24 13:55:35 +0900

    add tedious

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,21 @@
 var express = require('express');
 var router = express.Router();
 
+var Connection = require('tedious').Connection;  
+var config = {  
+    userName: 'imagire',  
+    password: 'P@ssword',  
+    server: '2016web2-imagire.database.windows.net',  
+    // If you are on Microsoft Azure, you need this:  
+    options: {encrypt: true, database: 'AdventureWorks'}  
+};  
+var connection = new Connection(config);  
+connection.on('connect', function(err) {  
+// If no error, then good to proceed.  
+    console.log("Connected");  
+});  
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
